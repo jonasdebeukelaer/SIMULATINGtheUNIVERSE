e153c23fd498bae750ae673afdb6c24ec21ded97
oliclipsham <oliverclipsham@yahoo.com>
2014-11-19 10:33:34 +0000

    Nearly there with smooshing Lena

--- fftwTutorial.py
+++ fftwTutorial.py
@@ -1,16 +1,40 @@
 import pyfftw
+import math
 import numpy as np
 from PIL import Image
+
+def xyGaussianArray(shape, xSigma, ySigma):
+
+	xSize = shape[1]
+	ySize = shape[0]
+
+	xCentre = (xSize / 2.0) - 0.5
+	yCentre = (ySize / 2.0) - 0.5
+
+	gaussianArray = np.zeros(shape)
+
+	for x in range(0, xSize):
+		for y in range(0, ySize):
+			gaussianArray[x][y] = math.exp(-(((x-xCentre)**2/2.0*xSigma**2)+((y-yCentre)**2/2.0*ySigma**2)))
+	
+	print gaussianArray
+	return gaussianArray
 
 lena = Image.open("lena_bw.bmp")
 lenaArray = np.array(lena)
 
-for i in range(0, 10000000):
-	lenaFFT = pyfftw.builders.fft(lenaArray)
-	transformLenaArray = lenaFFT()
+gaussianSmoother = xyGaussianArray(lenaArray.shape, 0.00001, 0.00001)
 
-	inverseLenaFFT = pyfftw.builders.ifft(transformLenaArray)
-	lenaArray = inverseLenaFFT()
+lenaFFT = pyfftw.builders.fft(lenaArray)
+lenaTransform = lenaFFT()
 
-smooshFaceLena = Image.fromarray(np.uint8(lenaArray))
+gaussianFFT = pyfftw.builders.fft(gaussianSmoother)
+gaussianTransform = gaussianFFT()
+
+fourierSmoothedLena = np.multiply(gaussianTransform, lenaTransform)
+
+inverseLenaFFT = pyfftw.builders.ifft(fourierSmoothedLena)
+smoothedLena = inverseLenaFFT()
+
+smooshFaceLena = Image.fromarray(np.uint8(smoothedLena))
 smooshFaceLena.save("Smoosh.bmp")
