e0d98a05a8b8b95ade3d43addffeb32e0b806b56
Jonas De Beukelaer <jonasdebeukelaer@Jonass-MacBook-Pro.local>
2014-11-25 23:49:38 +0000

    hack implemented. there isnt, and never was, a problem.

--- fftwTutorial.py
+++ fftwTutorial.py
@@ -3,6 +3,7 @@ import math
 import numpy as np
 from scipy import signal
 from PIL import Image
+import time
 
 def xyGaussianArray(shape, xSigma, ySigma):
 
@@ -30,7 +31,7 @@ def xyGaussianArray(shape, xSigma, ySigma):
 lena = Image.open("lena_bw.bmp")
 lenaArray = np.array(lena)
 
-gaussianSmoother = xyGaussianArray(lenaArray.shape, 5, 5)
+gaussianSmoother = xyGaussianArray(lenaArray.shape, 2, 2)
 
 lenaFFT     = pyfftw.builders.rfftn(lenaArray)
 gaussianFFT = pyfftw.builders.rfftn(gaussianSmoother)
@@ -39,15 +40,18 @@ gaussianTransform = gaussianFFT()
 lenaTransform = lenaFFT()
 print gaussianTransform.shape
 print lenaTransform.shape
-fourierSmoothedLena = (gaussianTransform.real * lenaTransform.real) + (gaussianTransform.imag * lenaTransform.imag) * 1j
 
-inverseFFT = pyfftw.builders.irfftn((fourierSmoothedLena))
+fourierSmoothedLena = gaussianTransform * lenaTransform
+
+inverseFFT = pyfftw.builders.irfftn(fourierSmoothedLena)
 inverseLenaFFT = inverseFFT()
+inverseLenaFFT = np.fft.fftshift(inverseLenaFFT)
 
 maxAmplitude = np.amax(inverseLenaFFT)
 inverseLenaFFT *= (256.0 / maxAmplitude)
 
-smooshFaceLena = Image.fromarray(np.uint8(inverseLenaFFT))
+smooshFaceLena = Image.fromarray(inverseLenaFFT.astype('uint8'))
+#smooshFaceLena.save("pic_archive/Smoosh_%s.bmp" % (time.strftime("%c")))
 smooshFaceLena.save("Smoosh.bmp")
 
 
