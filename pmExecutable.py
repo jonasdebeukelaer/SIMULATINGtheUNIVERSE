333b794c3475230f430fb6d5507416f8d5c455c2
oliclipsham <oliverclipsham@yahoo.com>
2014-12-28 17:20:07 +0000

    Cleaned up, modularised, doesn't quite work currently due to lack of boundary conditions

---
+++ pmExecutable.py
@@ -0,0 +1,108 @@
+import pmHeader as pm
+
+import numpy as np
+import math
+import os
+import sys
+import random
+import time
+import pyfftw
+from pync import Notifier
+
+start = time.time()
+print "Seeding..."
+random.seed(89321)
+print "Done\n"
+
+#------------INITIALISATION PARAMETERS-----------#
+
+volume                   = [100, 100, 100]
+initialisationResolution = 0.1
+gridResolution           = 1
+
+numParticles             = 100
+positionDistribution     = 0
+velocityDistribution     = 1
+maxVelocity              = 1
+hasCenterParticle        = False
+
+numTimeSteps             = 1000
+timeStepSize             = 0.01
+shootEvery               = 100
+
+#------------------------------------------------#
+
+#------------INITIALISATION FUNCTIONS------------#
+
+print "Initialising particles..."
+particleList = pm.InitialiseParticles(volume, initialisationResolution, numParticles, positionDistribution, velocityDistribution, maxVelocity)
+if hasCenterParticle:
+	centreParticle = pm.Particle([0., 0., 0.], [0., 0., 0.,], 20)
+	particleList.append(centreParticle)
+	numParticles += 1
+print"Done\n"
+
+print "Determining mesh shape..."
+densityField = pm.CalculateDensityField(volume, gridResolution, particleList, False)
+print "Done\n"
+
+print "Calculating Green's function..."
+greensFunction = pm.CreateGreensFunction(densityField.shape)
+print "Done\n"
+
+if os.path.exists("Results/values_frame0.3D"):
+	raw_input("Delete yo motherflippin results from the last test, you simpleton! Or, if you'rereally sure, just hit enter I guess...\n")
+
+#------------------------------------------------#
+
+#-----------------ITERATION LOOP-----------------#
+
+print "Iterating..."
+timeStep = 0
+while timeStep < numTimeSteps:
+
+	pm.OutputPercentage(timeStep, numTimeSteps)
+
+	shoot = True if (timeStep % shootEvery) == 0 else False
+	if shoot:
+		f = open("Results/values_frame%d.3D" % (timeStep), "w")
+		f.write("x y z VelocityMagnitude\n")
+
+	densityField   = pm.CalculateDensityField(volume, gridResolution, particleList)
+	potentialField = pm.SolvePotential(densityField, greensFunction, timeStep)
+
+	for particle in particleList:
+
+		particleAcceleration = pm.CalculateParticleAcceleration(particle, potentialField, gridResolution)
+		if timeStep == 0:
+			particle.acceleration = particleAcceleration
+
+		particle.position     += np.multiply(timeStepSize, particle.velocity) + np.multiply(0.5 * timeStepSize**2, particle.acceleration)
+		particle.velocity     -= np.multiply((0.5 * timeStepSize), (np.add(particle.acceleration, particleAcceleration))) # THIS IS UBER WRONG
+		velocityMagnitude     =  ((particle.velocity[0])**2 + (particle.velocity[1])**2 + (particle.velocity[2])**2)
+		particle.acceleration =  particleAcceleration
+
+		if shoot:
+			f.write("%f %f %f %f\n" % (particle.position[0], particle.position[1], particle.position[2], velocityMagnitude))
+
+	if shoot:
+		f.write("%f %f %f %f\n%f %f %f %f\n" % (volume[0] / 2, volume[1] / 2, volume[2] / 2, 0., - volume[0] / 2, - volume[1] / 2, - volume[2] / 2, 0.))
+		f.close()
+
+	timeStep += 1
+
+	if timeStep == numTimeSteps:
+		Notifier.notify('%d time steps complete' % (numTimeSteps), title = 'User input required')
+		moreSteps = raw_input("\n\nPlease check your VisIt output... would you like to add more time steps         (currently %d run)? (y/n): " % (numTimeSteps))
+		if moreSteps == 'y':
+			extraSteps = int(raw_input("\nInput desired number of extra steps: "))
+			sys.stdout.write("\n")
+			numTimeSteps += extraSteps
+
+#------------------------------------------------#
+
+end = time.time()
+sys.stdout.write("\n")
+sys.stdout.write("Simulation time = %fs\n\n" % (end - start))
+
+Notifier.notify('The universe has been solved', title = 'Thanks to the finest minds of the 21st century...')
---
+++ pmHeader.py
@@ -0,0 +1,158 @@
+import numpy as np
+import math
+import os
+import sys
+import random
+import time
+import pyfftw
+from pync import Notifier
+
+G = 1
+
+class Particle:
+	def __init__(self, position, velocity, mass):
+		self.position     = position
+		self.velocity     = velocity
+		self.mass         = mass
+		self.acceleration = [0.0, 0.0, 0.0]
+
+def InitialiseParticles(volume, initialisationResolution, numParticles, positionDistribution, velocityDistribution, maxVelocity):
+	particleList = []
+	
+	for i in range(0, numParticles):
+		newParticle = Particle([0., 0., 0.] ,[0., 0., 0.], 1)
+		particleList.append(newParticle)
+
+	# Random coordinates distribution
+	if positionDistribution == 0:
+		for particle in particleList:
+			x                 = random.randrange((- volume[0] / 2) / initialisationResolution, (volume[0] / 2) / initialisationResolution) * initialisationResolution
+			y                 = random.randrange((- volume[1] / 2) / initialisationResolution, (volume[1] / 2) / initialisationResolution) * initialisationResolution
+			z                 = random.randrange((- volume[2] / 2) / initialisationResolution, (volume[2] / 2) / initialisationResolution) * initialisationResolution
+			particle.position = [x, y, z]
+
+	# Random shell of particles distribution
+	elif positionDistribution == 1:
+		r = min([volume[0] / 2, volume[1] / 2, volume[2] / 2])
+		for particle in particleList:
+			theta = math.acos(random.uniform(-1., 1.))
+			phi   = random.uniform(0., 2 * math.pi)
+			
+			x     = r * math.sin(theta) * math.cos(phi)
+			y     = r * math.sin(theta) * math.sin(phi)
+			z     = r * math.cos(theta)
+
+			particle.position = [x, y, z]
+
+	# Even shell of particles distribution
+	elif positionDistribution == 2:
+		r            = min([volume[0] / 2, volume[1] / 2, volume[2] / 2])
+		
+		golden_angle = np.pi * (3 - np.sqrt(5))
+		theta        = golden_angle * np.arange(numParticles)
+		z            = np.linspace(r - 1.0 / numParticles, 1.0 / numParticles - r, numParticles)
+		radius       = np.sqrt((r**2 - z * z))
+		 
+		points       = np.zeros((numParticles, 3))
+		points[:,0]  = radius * np.cos(theta)
+		points[:,1]  = radius * np.sin(theta)
+		points[:,2]  = z
+
+		i = 0
+		for particle in particleList:
+			particle.position = points[i, :]
+			i += 1
+
+	else:
+		print 'Invalid position distribution selected'
+
+	# Random velocity distribution
+	if velocityDistribution == 0:
+		for particle in particleList:
+			xVelocity         = random.randrange(- maxVelocity / initialisationResolution, maxVelocity / initialisationResolution) * initialisationResolution
+			yVelocity         = random.randrange(- maxVelocity / initialisationResolution, maxVelocity / initialisationResolution) * initialisationResolution
+			zVelocity         = random.randrange(- maxVelocity / initialisationResolution, maxVelocity / initialisationResolution) * initialisationResolution
+			particle.velocity = [xVelocity, yVelocity, zVelocity]
+
+	# Zero velocity distribution
+	elif velocityDistribution == 1:
+		for particle in particleList:
+			particle.velocity = [0, 0, 0]
+
+	else:
+		print 'Invalid velocity distribution selected'
+
+	return particleList
+
+def FindMeshIndex(position, gridResolution, gridSize): 
+	return round((position / gridResolution) + (gridResolution / 2)) + ((gridSize / 2) - 1)
+
+def CalculateDensityField(volume, gridResolution, particleList, populateArray = True):
+	meshShape = [volume[0] / gridResolution, volume[1] / gridResolution, volume[2] / gridResolution]
+	if meshShape[0] != int(meshShape[0]) or meshShape[1] != int(meshShape[1]) or meshShape[2] != int(meshShape[2]):
+		sys.exit("Error: non-integer cell number defined pleuz fix")
+
+	densityFieldMesh = np.zeros((meshShape))
+
+	if populateArray:
+		for particle in particleList:
+			xMesh = FindMeshIndex(particle.position[0], gridResolution, meshShape[0])
+			yMesh = FindMeshIndex(particle.position[1], gridResolution, meshShape[1])
+			zMesh = FindMeshIndex(particle.position[2], gridResolution, meshShape[2])
+
+			densityFieldMesh[xMesh][yMesh][zMesh] += particle.mass
+
+		densityFieldMesh /= (gridResolution**3)
+
+	return densityFieldMesh
+
+def CreateGreensFunction(unalteredShape):
+	shape       = (unalteredShape[0], unalteredShape[1], (unalteredShape[2] / 2) + 1)
+	greensArray = np.zeros((shape))
+
+	constant = 1
+
+	for l in range(0, shape[0]):
+		if l <= shape[0] / 2:
+			kx = 2 * math.pi * l / (shape[0] - 1)
+		else:
+			kx = 2 * math.pi * (l - shape[0]) / (shape[0] - 1)
+
+		for m in range(0, shape[1]):
+			if m <= shape[1] / 2:
+				ky = 2 * math.pi * m / (shape[1] - 1)
+			else:
+				ky = 2 * math.pi * (m - shape[1]) / (shape[1] - 1)
+
+			for n in range(0, shape[2]):
+				kz = math.pi * n / (shape[2] - 1)
+
+				if l != 0 or m != 0 or n != 0:
+					greensArray[l][m][n] = - constant / ((math.sin(kx * 0.5))**2 + (math.sin(ky * 0.5))**2 + (math.sin(kz * 0.5))**2)
+
+	return greensArray
+
+def SolvePotential(densityField, greensFunction, timeStep):
+	densityFieldFFT = pyfftw.builders.rfftn(densityField)
+	densityFieldConvoluted = densityFieldFFT() * greensFunction
+	potentialFieldJumbled = pyfftw.builders.irfftn(densityFieldConvoluted)
+	potentialField = np.fft.fftshift(potentialFieldJumbled())
+	return potentialField
+
+def CalculateParticleAcceleration(particle, potentialField, gridResolution):
+	meshShape = potentialField.shape
+
+	xMesh = FindMeshIndex(particle.position[0], gridResolution, meshShape[0])
+	yMesh = FindMeshIndex(particle.position[1], gridResolution, meshShape[1])
+	zMesh = FindMeshIndex(particle.position[2], gridResolution, meshShape[2])
+
+	xAcceleration = - (potentialField[xMesh + 1][yMesh][zMesh] - potentialField[xMesh - 1][yMesh][zMesh]) / 2.0
+	yAcceleration = - (potentialField[xMesh][yMesh + 1][zMesh] - potentialField[xMesh][yMesh - 1][zMesh]) / 2.0
+	zAcceleration = - (potentialField[xMesh][yMesh][zMesh + 1] - potentialField[xMesh][yMesh][zMesh - 1]) / 2.0
+
+	return (xAcceleration, yAcceleration, zAcceleration)
+
+def OutputPercentage(timeStep, numTimeSteps):
+	i = (float(timeStep + 1) / numTimeSteps) * 100
+	sys.stdout.write("\r%.2f%%" % i)
+	sys.stdout.flush()
