2d020e38d4770701deb61d98bfe7f28650906fb4
oliclipsham <oliverclipsham@yahoo.com>
2015-03-25 11:38:03 +0000

    Valid value of h for mass calculation

--- pmMassCalculate.py
+++ pmMassCalculate.py
@@ -1,7 +1,7 @@
 nGrid = float(raw_input("Please input nGrid: "))
 lBox  = float(raw_input("Please input lBox: "))
 
-h = 1
+h = 0.7
 
 totalMass = h**2 * 2.79 * 10**11 * lBox**3
 particleMass = totalMass / (nGrid**3)
